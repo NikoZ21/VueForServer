b350504c053e38557d5676047e282d65fdd837f0
NikoZ21 <89398005+NikoZ21@users.noreply.github.com>
2024-04-09 16:43:08 +0400

    Init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <h2>counter {{ counter }}</h2>
+  <button @click="counter++">+</button>
+  <button @click= "counter--">-</button>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      counter : 0
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
